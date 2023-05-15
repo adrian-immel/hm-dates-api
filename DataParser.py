54240dcd15eb541d8a65338b3212a68db59f6e73
Adrian <adrian.immel@outlook.de>
2023-05-15 12:26:01 +0200

    Fix WebParser.py & searchandappend function

--- DataParser.py
+++ DataParser.py
@@ -1,17 +1,14 @@
 import json
-import re
 
 
 def jsonparser(object_array):
     return json.dumps(object_array, indent=4)
 
 
-def searchandappend(list_to_search, string_to_search, array_to_append):
-    i = list_to_search.indexOf(string_to_search)
+def searchandappend(list_to_search, string_to_search):
+    return_array = []
+    for i in list_to_search:
+        if string_to_search in i[0]:
+            return_array = i
     # todo format the output and and clean output
-    array_to_append.append(list_to_search[i:2])
-    return array_to_append
-
-
-class DataParser:
-    pass
+    return return_array
--- WebParser.py
+++ WebParser.py
@@ -10,6 +10,7 @@ class WebParser:
         page = requests.get(url)
         # needs to be done to fix wrong table annotation of webpage to work properly with BeautifulSoup
         page = page.content.replace(b"/th", b"/td")
+#        page = page.replace(b"/br", b"\n")
         soup = BeautifulSoup(page, 'html.parser')
         # find the div with the id "termine"
         self.page_element = soup.find("div", {"id": "termine"})
@@ -18,10 +19,11 @@ class WebParser:
         # extract the Name of the Semester from the button
         semester_type = [i.text for i in self.page_element.findAll('button', class_='accordion-button collapsed')][
             table_selector]
-        return semester_type
+        return semester_type.strip('\n')
 
     def getdates(self, table_selector):
         # find all Tables in the div
+
         table = self.page_element.find_all('table')[table_selector]
 
         # extract the cells
@@ -30,7 +32,9 @@ class WebParser:
         # iterate through cells and insert data in array
         for row in rows:
             cols = row.find_all('td')
-            cols = [col.text.strip() for col in cols]
-            return_array.append(cols)
+            row = []
+            for col in cols:
+                cols = col.get_text(strip=True, separator='\n').splitlines()
+                row.append(cols)
+            return_array.append(row)
         return return_array
-
