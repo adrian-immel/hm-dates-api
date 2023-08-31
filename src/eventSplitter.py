8c3b0e99894f0d1f85fbe06a9209901d77bf1396
Moritz Huber <2nd@moritzhuber.de>
2023-08-31 14:30:19 +0000

    zusätzlich events mit mehreren daten aufgeteilt

---
+++ src/eventSplitter.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+import jsonpickle
+
+
+def split_events(data: list):
+    events = []
+
+    for event in data:
+        events.extend(event.split())
+        filename = (sys.path[0]+r'/../dates-api/thisSemester/split.json')
+        with open(filename, 'w') as f:
+            f.write(jsonpickle.encode(events, unpicklable=False, indent=4))
