f13a89053c23ce90f092a15f9c12010179fd87d2
Adrian Immel <76220205+adriangeorgimmel@users.noreply.github.com>
2023-10-12 18:16:45 +0200

    Hotfix to change to next semester if the end of the current semester duration is in the past

--- src/main.py
+++ src/main.py
@@ -21,6 +21,11 @@ def job():
         semester_name_thisSemester = web_parser.get_semester_type(0)
         event_list_nextSemester = event_creator(web_parser.get_dates(1))
         semester_type_nextSemester = web_parser.get_semester_type(1)
+        # Hotfix to change to next semester if the end of the current semester duration is in the past
+        for event in event_list_thisSemester:
+            if event.title == "Dauer des Semesters" and event.dates[0].end < datetime.now():
+                event_list_thisSemester = event_list_nextSemester
+        
         json_object_assembler(event_list_thisSemester, "thisSemester", semester_name_thisSemester)
         json_object_assembler(event_list_nextSemester, "nextSemester", semester_type_nextSemester)
         create_ical(event_list_thisSemester)
